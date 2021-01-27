04844973c394a9eb1c28a139a7050eb945d17034
hodeng28 <ghepd28@gmail.com>
2021-01-28 00:06:01 +0900

    init antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import './App.css';
 import MainRouter from './Router/MainRouter';
 import Nav from './Components/Nav/Nav';
+import styles from './App.module.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div>
-      <h1>NAVER DATALab.</h1>
+      <h1 className={styles.app}>NAVER <strong>DATALab.</strong></h1>
       <Nav />
       <hr />
       <MainRouter />
+
+      <Button type="primary">Hello, Ant Design!</Button>
+      <a href="#">I'm a link. Click me, please!</a>
     </div>
   );
 }
---
+++ src/Components/Search/Search.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <>
+      <h1>급상승검색어</h1>
+      <div>테스트</div>  
+      <div>
+        <ul>
+          <li>10대</li>
+          <li>20대</li>
+          <li>30대</li>
+          <li>40대</li>
+          <li>50대-</li>
+          <li>전체</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+
+export default Search;
