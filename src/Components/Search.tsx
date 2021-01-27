d138a6c5e5af92b5d2e0371c76492c47a53455a0
daisy(정다혜) <daisy.love@kakaocommerce.com>
2021-01-28 00:25:39 +0900

    데이터랩 api 호출

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,14 +1,56 @@
 import React from "react";
+import axios from "axios";
 
+class Search extends React.Component {
+  getData = async () => {
+    const request_body = {
+      startDate: "2017-01-01",
+      endDate: "2017-04-30",
+      timeUnit: "month",
+      keywordGroups: [
+        {
+          groupName: "한글",
+          keywords: ["한글", "korean"],
+        },
+        {
+          groupName: "영어",
+          keywords: ["영어", "english"],
+        },
+      ],
+      device: "pc",
+      gender: "f",
+    };
 
-const Search = () => {
-  return (
-    <>
-      <h1>급상승검색어</h1>
-      <div>테스트</div>
-    </>
-  )
+    const ID_KEY = "PBLIpouYn0jTczwJITLA";
+    const SECRET_KEY = "SxTNaVSodJ";
+
+    try {
+      const data = await axios({
+        method: "post",
+        url: "/v1/datalab/search",
+        data: request_body,
+        headers: {
+          "X-Naver-Client-Id": ID_KEY,
+          "X-Naver-Client-Secret": SECRET_KEY,
+        },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <>
+        <h1>급상승검색어</h1>
+        <div>테스트</div>
+      </>
+    );
+  }
 }
-
-
 export default Search;
