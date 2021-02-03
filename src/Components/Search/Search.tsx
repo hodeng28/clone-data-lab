130c2f2d08b73d37498c036da3af59e546f1224a
hodeng28 <ghepd28@gmail.com>
2021-02-04 00:45:06 +0900

    merge

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,38 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { getData } from '../../Api/api';
+import { useEffect } from "react";
+import { getData } from '../../api/searchApi';
 
-
-const Search = () => {
-
-  const initialState = []
-
-  const [state, setState] = useState(initialState);
+const Search = ()=> {
 
   useEffect(() => {
-    //프로미스를 반환하므로 함수 형태로 준비를 시킨 후
-    const bookFunc = async () => {
-      let naverData = await getData.request_body();
-
-      console.log(naverData);
-    };
-    //하단에서 실행시켜줍니다!
-    bookFunc();
-  }, []);
-
+    getData();
+  }, [])
 
   return (
     <>
         <h1>급상승검색어</h1>
-        <div>테스트</div>  
-        <div>
-          <ul>
-            <li>10대</li>
-            <li>20대</li>
-            <li>30대</li>
-            <li>40대</li>
-            <li>50대-</li>
-            <li>전체</li>
-          </ul>
+    <div>테스트</div>  
+    <div>
+      <ul>
+        <li>10대</li>
+        <li>20대</li>
+        <li>30대</li>
+        <li>40대</li>
+        <li>50대-</li>
+        <li>전체</li>
+      </ul>
     </div>
     </>
   );
