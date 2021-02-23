8226441a7f4f6683ef912f6c872c4e954bc10f2a
daisy(정다혜) <daisy.love@kakaocommerce.com>
2021-02-23 21:24:33 +0900

    네비게이션메뉴 적용

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,25 +1,31 @@
-import React from "react";
+import { Menu } from "antd";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Nav = () => {
+  const [current, setCurrent] = useState("home");
+  const handleClick = (e: any) => {
+    setCurrent(e.key);
+  };
+
   return (
     <>
-      <ul>
-        <li>
+      <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="home">
           <NavLink exact to="/">
             데이터랩 홈
           </NavLink>
-        </li>
-        <li>
+        </Menu.Item>
+        <Menu.Item key="search">
           <NavLink to="/search">급상승 검색어</NavLink>
-        </li>
-        <li>
+        </Menu.Item>
+        <Menu.Item key="statistics">
           <NavLink to="/Statistics">지역통계</NavLink>
-        </li>
-        <li>
+        </Menu.Item>
+        <Menu.Item key="ageTrend">
           <NavLink to="/AgeTrend">월별 트렌드-데이지</NavLink>
-        </li>
-      </ul>
+        </Menu.Item>
+      </Menu>
     </>
   );
 };
